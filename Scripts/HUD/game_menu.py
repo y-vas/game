3277fa7a8c6c319a684d85f9b988f6e51463408c
Vasyl <yovdiyvasyl@gmail.com>
2019-02-26 17:42:03 +0100

    asdfads

--- Scripts/HUD/game_menu.py
+++ Scripts/HUD/game_menu.py
@@ -1,9 +1,8 @@
 
 
 
-def menu(camera):
-    print("loading OK")
-    print(camera)
+def menu(L):
+    camera = L.getCurrentScene().active_camera
 
     # A sphere of radius 4.0 located at [x, y, z] = [1.0, 1.0, 1.0]
     if (camera.sphereInsideFrustum([1.0, 1.0, 1.0], 4) != camera.OUTSIDE):
--- Scripts/run_game.py
+++ Scripts/run_game.py
@@ -2,19 +2,8 @@ import sys, os, bpy, imp
 
 
 directory = os.path.dirname(bpy.data.filepath)
-appends = [
-[os.path.join(directory, "Scripts","Movement","player_movement.py"), True],
-[os.path.join(directory, "Scripts","HUD","game_menu.py"), True],
-[os.path.join(directory, "Scripts","maiutils.py"), False]
-]
 
-sources = []
-for file in appends:
-    if file[1] == True:
-        print("importing: "+file[0])
-        sources.append(imp.load_source('', file[0]))
-
-game_playng = sources[0];
+game_menu = imp.load_source('', os.path.join(directory, "Scripts","HUD","game_menu.py") );
 
 from bge import logic as L
 from bge import events
@@ -22,9 +11,8 @@ from bge import events
 L.addScene("Hud",1)
 
 scene = L.getCurrentScene()
-HUD = L.getSceneList()[1]
 
-
+HUD = L.getSceneList()
 
 SO = scene.objects;
 
@@ -39,8 +27,9 @@ def run():
     global RUN_TYPE
 
     if RUN_TYPE == 0:
-        game_playng.testdef()
+        # game_playng.testdef()
+        pass
     elif RUN_TYPE == 1:
-        print(HUD)
+        print(L.getSceneList()[1])
 
-        # sources[1].menu(L.getCurrentScene().active_camera);
+        # game_menu.menu(L.getCurrentScene().active_camera);
